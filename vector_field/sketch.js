6530b5b87528e5becccd4aaa956e2af7bbd0d1a8
Tom Crasset <tomcrasset@protonmail.com>
2021-03-23 12:42:15 +0100

    Fix format of vector_field

--- vector_field/sketch.js
+++ vector_field/sketch.js
@@ -1,24 +1,30 @@
-let scale = 10;
-let cols = 50;
-let rows = 50;
+const scale = 10
+const cols = 50
+const rows = 50
 const noisiness = 0.002
 let zoff = 0
 
-var gridCanvas;
+let gridCanvas
 
+// eslint-disable-next-line no-unused-vars
 function setup() {
-  gridCanvas = new GridCanvas(rows, cols, scale);
-  gridCanvas.createCanvas();
+    gridCanvas = new GridCanvas(rows, cols, scale)
+    gridCanvas.createCanvas()
 }
 
 function createUnitVector(x, y, scale) {
-  const angle = noise(x*noisiness,y*noisiness, zoff);
-  myVec = new DisplayUnitVector(angle, 1, scale, 1);
-  return myVec.show(x, y, scale);
+    const angle = noise(x * noisiness, y * noisiness, zoff)
+    myVec = new DisplayUnitVector(angle, 1, scale, 1)
+    return myVec.show(x, y, scale)
 }
 
+// eslint-disable-next-line no-unused-vars
 function draw() {
-  background(255)
-  gridCanvas.draw2DGrid(shapeCallback=createUnitVector, null, strokeColor = 0);
-  zoff += 0.003
+    background(255)
+    gridCanvas.draw2DGrid(
+        (shapeCallback = createUnitVector),
+        null,
+        (strokeColor = 0)
+    )
+    zoff += 0.003
 }
