5987940b05d2ac6f474df899f71d3af910158d49
Tom Crasset <tomcrasset@protonmail.com>
2021-03-23 12:54:43 +0100

    Use tabWidth 2

--- vector_field/sketch.js
+++ vector_field/sketch.js
@@ -8,23 +8,23 @@ let gridCanvas
 
 // eslint-disable-next-line no-unused-vars
 function setup() {
-    gridCanvas = new GridCanvas(rows, cols, scale)
-    gridCanvas.createCanvas()
+  gridCanvas = new GridCanvas(rows, cols, scale)
+  gridCanvas.createCanvas()
 }
 
 function createUnitVector(x, y, scale) {
-    const angle = noise(x * noisiness, y * noisiness, zoff)
-    myVec = new DisplayUnitVector(angle, 1, scale, 1)
-    return myVec.show(x, y, scale)
+  const angle = noise(x * noisiness, y * noisiness, zoff)
+  myVec = new DisplayUnitVector(angle, 1, scale, 1)
+  return myVec.show(x, y, scale)
 }
 
 // eslint-disable-next-line no-unused-vars
 function draw() {
-    background(255)
-    gridCanvas.draw2DGrid(
-        (shapeCallback = createUnitVector),
-        null,
-        (strokeColor = 0)
-    )
-    zoff += 0.003
+  background(255)
+  gridCanvas.draw2DGrid(
+    (shapeCallback = createUnitVector),
+    null,
+    (strokeColor = 0)
+  )
+  zoff += 0.003
 }
