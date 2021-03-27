c213ed28d8b861d9b16873273d44658f3f875f68
Tom Crasset <tomcrasset@protonmail.com>
2021-03-27 08:48:01 +0100

    Move gridCanvas methods out of constructor

--- src/core/gridCanvas.js
+++ src/core/gridCanvas.js
@@ -57,125 +57,118 @@ class GridCanvas {
 
     this.arrayLength = rows * cols
     this.grid = Array(this.arrayLength)
-    this.createCanvas = function () {
-      p5.createCanvas(this.totalHeight, this.totalWidth, p5.P2D)
-    }
+  }
+  createCanvas() {
+    this.p5.createCanvas(this.totalHeight, this.totalWidth, p5.P2D)
+  }
 
-    this.createGrid = function (cellCallback = required()) {
-      for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-          const index = this.cellIndexToArrayIndex(i, j)
-          this.grid[index] = cellCallback(i, j, scale)
-        }
-      }
-      return this.grid
-    }
-
-    this.draw2DGrid = function (shapeCallback = required()) {
-      // Draws the 2D grid with an object returned by `shapeCallback`
-      for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-          const cellCoordinates = GridCanvas.cellIndexToCoordinates(
-            i,
-            j,
-            this.scale,
-            this.p5
-          )
-          shapeCallback(cellCoordinates.x, cellCoordinates.y, scale)
-        }
-      }
-    }
-
-    this.valid_i = function (i = required()) {
-      return i >= 0 && i < this.rows
-    }
-
-    this.valid_j = function (j = required()) {
-      return j >= 0 && j < this.cols
-    }
-
-    this.checkOOBIndex = function (i = required(), j = required()) {
-      const coords = GridCanvas.cellIndexToCoordinates(i, j, this.scale, p5)
-      this.checkOOBCoords(coords.x, coords.y)
-    }
-
-    this.checkOOBCoords = function (x = required(), y = required()) {
-      if (x >= this.totalHeight || x < 0 || y >= this.totalWidth || y < 0) {
-        throw Error(
-          `Position out of bounds. Actual(${x}, ${y}), Min is (0,0), Max is (${this.totalWidth}, ${this.totalHeight}) `
-        )
-      }
-    }
-
-    this.neighbours = function (
-      i = required(),
-      j = required(),
-      withDiagonals = false
-    ) {
-      const neighbours = []
-
-      if (this.valid_i(i - 1)) neighbours.push(this.p5.createVector(i - 1, j))
-      if (this.valid_j(j - 1)) neighbours.push(this.p5.createVector(i, j - 1))
-      if (this.valid_j(j + 1)) neighbours.push(this.p5.createVector(i, j + 1))
-      if (this.valid_i(i + 1)) neighbours.push(this.p5.createVector(i + 1, j))
-
-      if (withDiagonals) {
-        // ! UPPER LEFT DIAGONAL
-        if (this.valid_i(i - 1) && this.valid_j(j - 1))
-          neighbours.push(this.p5.createVector(i - 1, j - 1))
-        // ! UPPER RIGHT DIAGONAL
-        if (this.valid_i(i + 1) && this.valid_j(j - 1))
-          neighbours.push(this.p5.createVector(i + 1, j - 1))
-        // ! LOWER LEFT DIAGONAL
-        if (this.valid_i(i - 1) && this.valid_j(j + 1))
-          neighbours.push(this.p5.createVector(i - 1, j + 1))
-        // ! LOWER RIGHT DIAGONAL
-        if (this.valid_i(i + 1) && this.valid_j(j + 1))
-          neighbours.push(this.p5.createVector(i + 1, j + 1))
-      }
-
-      return neighbours
-    }
-
-    this.cellIndexToArrayIndex = function (i = required(), j = required()) {
-      this.checkOOBIndex(i, j)
-      return i * this.cols + j
-    }
-
-    this.arrayIndexToCellIndex = function (index = required()) {
-      if (index >= this.arrayLength || index < 0) {
-        throw Error(`Index (${index}) out of bounds`)
-      }
-      const x = index % this.cols // % is the "modulo operator", the remainder of i / width;
-      const y = index / this.cols
-      return this.p5.createVector(x, y)
-    }
-
-    this.getGridElementAtCellIndex = function (i = required(), j = required()) {
-      try {
-        // When called as a first function (i.e. not within this.getGridElementAtCoordinate()),
-        // we want the user to know that he selected an out of bounds index.
+  createGrid(cellCallback = required()) {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
         const index = this.cellIndexToArrayIndex(i, j)
-        return this.grid[index]
-      } catch (error) {
-        console.error(
-          `Index (${i}, ${j}) out of bounds. Min is (0,0). 
-          Max is (${cols - 1}, ${rows - 1})`
-        )
+        this.grid[index] = cellCallback(i, j, this.scale)
       }
     }
+    return this.grid
+  }
 
-    this.getGridElementAtCoordinate = function (
-      x = required(),
-      y = required()
-    ) {
-      // Given a valid `x` and `y` on the canvas, returns the
-      // element at these coordinates
-
-      this.checkOOBCoords(x, y)
-
-      const cellIndex = GridCanvas.cellIndex(x, y, this.scale)
-      return this.getGridElementAtCellIndex(cellIndex.x, cellIndex.y)
+  draw2DGrid(shapeCallback = required()) {
+    // Draws the 2D grid with an object returned by `shapeCallback`
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        const cellCoordinates = GridCanvas.cellIndexToCoordinates(
+          i,
+          j,
+          this.scale,
+          this.p5
+        )
+        shapeCallback(cellCoordinates.x, cellCoordinates.y, this.scale)
+      }
     }
+  }
+
+  isValidI(i = required()) {
+    return i >= 0 && i < this.rows
+  }
+
+  isValidJ(j = required()) {
+    return j >= 0 && j < this.cols
+  }
+
+  checkOOBIndex(i = required(), j = required()) {
+    const coords = GridCanvas.cellIndexToCoordinates(i, j, this.scale, p5)
+    this.checkOOBCoords(coords.x, coords.y)
+  }
+
+  checkOOBCoords(x = required(), y = required()) {
+    if (x >= this.totalHeight || x < 0 || y >= this.totalWidth || y < 0) {
+      throw Error(
+        `Position out of bounds. Actual(${x}, ${y}), Min is (0,0), Max is (${this.totalWidth}, ${this.totalHeight}) `
+      )
+    }
+  }
+
+  neighbours(i = required(), j = required(), withDiagonals = false) {
+    const neighbours = []
+
+    if (this.isValidI(i - 1)) neighbours.push(this.p5.createVector(i - 1, j))
+    if (this.isValidJ(j - 1)) neighbours.push(this.p5.createVector(i, j - 1))
+    if (this.isValidJ(j + 1)) neighbours.push(this.p5.createVector(i, j + 1))
+    if (this.isValidI(i + 1)) neighbours.push(this.p5.createVector(i + 1, j))
+
+    if (withDiagonals) {
+      // ! UPPER LEFT DIAGONAL
+      if (this.isValidI(i - 1) && this.isValidJ(j - 1))
+        neighbours.push(this.p5.createVector(i - 1, j - 1))
+      // ! UPPER RIGHT DIAGONAL
+      if (this.isValidI(i + 1) && this.isValidJ(j - 1))
+        neighbours.push(this.p5.createVector(i + 1, j - 1))
+      // ! LOWER LEFT DIAGONAL
+      if (this.isValidI(i - 1) && this.isValidJ(j + 1))
+        neighbours.push(this.p5.createVector(i - 1, j + 1))
+      // ! LOWER RIGHT DIAGONAL
+      if (this.isValidI(i + 1) && this.isValidJ(j + 1))
+        neighbours.push(this.p5.createVector(i + 1, j + 1))
+    }
+
+    return neighbours
+  }
+
+  cellIndexToArrayIndex(i = required(), j = required()) {
+    this.checkOOBIndex(i, j)
+    return i * this.cols + j
+  }
+
+  arrayIndexToCellIndex(index = required()) {
+    if (index >= this.arrayLength || index < 0) {
+      throw Error(`Index (${index}) out of bounds`)
+    }
+    const x = index % this.cols // % is the "modulo operator", the remainder of i / width;
+    const y = index / this.cols
+    return this.p5.createVector(x, y)
+  }
+
+  getGridElementAtCellIndex(i = required(), j = required()) {
+    try {
+      // When called as a first function (i.e. not within this.getGridElementAtCoordinate()),
+      // we want the user to know that he selected an out of bounds index.
+      const index = this.cellIndexToArrayIndex(i, j)
+      return this.grid[index]
+    } catch (error) {
+      console.error(
+        `Index (${i}, ${j}) out of bounds. Min is (0,0). 
+          Max is (${cols - 1}, ${rows - 1})`
+      )
+    }
+  }
+
+  getGridElementAtCoordinate(x = required(), y = required()) {
+    // Given a valid `x` and `y` on the canvas, returns the
+    // element at these coordinates
+
+    this.checkOOBCoords(x, y)
+
+    const cellIndex = GridCanvas.cellIndex(x, y, this.scale)
+    return this.getGridElementAtCellIndex(cellIndex.x, cellIndex.y)
   }
 }
